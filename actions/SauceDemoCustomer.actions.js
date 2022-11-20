d194cd69dcdc33bf7e187ba54d0011660862672a
Fernando Tavora <fertavora@gmail.com>
2022-11-20 21:25:28 +0100

    removed return

--- actions/SauceDemoCustomer.actions.js
+++ actions/SauceDemoCustomer.actions.js
@@ -11,17 +11,14 @@ exports.SauceDemoCustomer = class SauceDemoCustomer {
     await this.page.locator('[data-test="username"]').fill('standard_user');
     await this.page.locator('[data-test="password"]').fill('secret_sauce');
     await this.page.locator('[data-test="login-button"]').click();
-    return this;
   }
 
   async addsProductToCart() {
     await this.page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-    return this;
   }
 
   async goesToCart() {
     await this.page.locator('a.shopping_cart_link').click();
-    return this;
   }
 
   async performsCheckout() {
@@ -31,12 +28,10 @@ exports.SauceDemoCustomer = class SauceDemoCustomer {
     await this.page.locator('[data-test="postalCode"]').fill(faker.address.zipCode());
     await this.page.locator('[data-test="continue"]').click();
     await this.page.locator('[data-test="finish"]').click();
-    return this;
   }
 
   async goesToHome() {
     await this.page.locator('[data-test="back-to-products"]').click();
-    return this;
   }
 
   async isAtInventoryPage() {
